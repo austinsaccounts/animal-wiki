4602044ba919856794018ae6fd35ce14c1850865
austin cummings and victoria martinez <austin@>
2018-08-09 11:28:03 -0700

    stylize the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
-  $("formnametobeinserted").submit(function(event) {
+
+  $("#animalSelection").submit(function(event) {
    event.preventDefault();
+   var animal = $("input:radio[name=optradio]:checked").val();
+   $(".big-pictures").hide();
+
+  if ( animal==="dolphin")
+  {
+   $('#dolphin').show();
+  }
+ if ( animal==="honeybadger")
+ {
+   $('#honeybadger').show();
+}
+if ( animal==="jellyfish")
+  {
+     $('#jellyfish').show();
+  }
+if ( animal==="octopus")
+  {
+     $('#octopus').show();
+  }
 });
+
+
+
 });
